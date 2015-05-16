832f288c19158c607c0cd9aed6cd133601a5f04b
fahmiderbali@gmail.com <fahmiderbali@gmail.com>
2015-05-16 19:08:22 -0400

    Enabling the debug bar for the project

--- config/app.php
+++ config/app.php
@@ -145,6 +145,7 @@ return [
 		'App\Providers\EventServiceProvider',
 		'App\Providers\RouteServiceProvider',
                 'Collective\Html\HtmlServiceProvider',
+                'Barryvdh\Debugbar\ServiceProvider',
 
 	],
 
