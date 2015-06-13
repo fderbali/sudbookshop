b63fb952cfac82e763f64aed8080df814d5cea6a
fahmiderbali@gmail.com <fahmiderbali@gmail.com>
2015-06-13 03:01:20 -0400

    add foreign key to category migration

--- database/migrations/2015_06_06_192103_create_category_table.php
+++ database/migrations/2015_06_06_192103_create_category_table.php
@@ -10,12 +10,20 @@ class CreateCategoryTable extends Migration {
 		Schema::create('category', function(Blueprint $table) {
 			$table->increments('id');
 			$table->string('name', 100);
-			$table->integer('section_id');
+			$table->integer('section_id')->unsigned();
+                        $table->foreign('section_id')
+                              ->references('id')
+                              ->on('section')
+                              ->onDelete('restrict')
+                              ->onUpdate('restrict');
 		});
 	}
 
 	public function down()
 	{
+            	Schema::table('category', function(Blueprint $table) {
+                    $table->dropForeign('category_section_id_foreign');
+		});
 		Schema::drop('category');
 	}
 }
